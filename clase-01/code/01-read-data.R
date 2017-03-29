7f82e217ac527120dd86ea679211052617a16585
jbkunst <jbkunst@gmail.com>
2017-03-29 00:38:35 -0300

    orden

--- clase-01/code/01-read-data.R
+++ clase-01/code/01-read-data.R
@@ -1,21 +1,51 @@
+# paquetes ----------------------------------------------------------------
+# esto se hace con CTRL + R
+# cargo los paquetes
+library(tidyverse) # ayuda al manejo datos map, select  
+library(stringr) # para manejos de stringr
 
-library(tidyverse)
-library(stringr)
 
+# lectura datos -----------------------------------------------------------
+dir("data/")
 
-files <- dir("data/", full.names = TRUE) 
+v <- c(1, 2, 3, 4)
+
+res1 <- log(v)
+res2 <- map(v, log)
+res3 <- map_dbl(v, log)
+
+res1 == res3
+identical(res1, res3)
+
+files <- dir("data/", full.names = TRUE)
+files
+
 texto <- map(files, readLines)
 
+texto
 
 map_chr(texto, 1)
 map_chr(texto, 2)
+
+# data frames
+data("mtcars")
+mtcars
+
+mtcars
+
+# data frames son tablas
+df <- data_frame(
+  col1 = c("var1", "var2"),
+  col2 = c(1, 3)
+)
+df
 
 data <- data_frame(
   file = files,
   prob = map_chr(texto, 1),
   texto = map_chr(texto, 2)
-)  
+) 
 
-data <- mutate(data, prob = as.numeric(prob))
 
-glimpse(data)
+write_tsv(data, "output/datos_casi_limpios.txt")
+
---
+++ clase-01/code/02-exploracion-data.R
@@ -0,0 +1,82 @@
+# paquetes ----------------------------------------------------------------
+rm(list = ls())
+library(tidyverse)
+library(stringr)
+library(tidytext)
+library(ggplot2)
+
+# data --------------------------------------------------------------------
+data <- read_tsv("output/datos_casi_limpios.txt")
+glimpse(data) # otra forma de ver al ojo lo que hay
+
+
+# explore -----------------------------------------------------------------
+data
+
+summarise(data, min(prob), max(prob))
+
+# grammar graphics
+ggplot(data) +
+  geom_histogram(aes(x = prob))
+
+ggplot(data) +
+   geom_point(aes(x = file, y = prob))
+
+data$texto
+
+datap <- unnest_tokens(data, palabra, texto)
+datap
+
+datac <- count(datap, palabra)
+datac <- arrange(datac, -n)
+datac
+
+
+# stopwords ---------------------------------------------------------------
+library(rvest)
+
+html <- read_html("http://www.ranks.nl/stopwords/spanish")
+stopwrods <- html_node(html, "table") 
+stopwrods <- html_text(stopwrods)
+stopwrods <- str_split(stopwrods, " ")
+stopwrods <- unlist(stopwrods)
+
+
+html <- read_html("https://es.wikipedia.org/wiki/Preposici%C3%B3n")
+dataprep <- html_table(html)[[1]]
+
+datac <- filter(datac, !palabra %in% stopwrods)
+datac <- filter(datac, !palabra %in% dataprep$Preposición)
+datac <- filter(datac, !palabra %in% c("que", "n"))
+datac <- filter(datac, n > 2)
+
+ggplot(datac) +
+  geom_col(aes(x = palabra, y = n))
+
+
+datas <- summarise(group_by(datap, palabra),
+          probmedia = mean(prob),
+          probmin = min(prob),
+          probmax = max(prob),
+          n = n())
+
+datas
+
+datas <- filter(datas, !palabra %in% stopwrods)
+datas <- filter(datas, !palabra %in% dataprep$Preposición)
+datas <- filter(datas, !palabra %in% c("que", "n"))
+datas <- filter(datas, n > 2)
+
+
+datas2 <- gather(datas, caract, valor, -palabra)
+datas2 <- filter(datas2, caract != "n")
+
+p <- ggplot(datas2) +
+  geom_line(aes(palabra, y = valor, group = caract, color = caract)) +
+  scale_color_manual(values = c("red", "blue", "green")) +
+  theme_minimal() +
+  ggtitle("asdas")
+
+p
+
+ggsave(p, filename = "nuestro_primer_grafico.pdf", width = 16, height = 9)
