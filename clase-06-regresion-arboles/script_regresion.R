96a6be257d59fa8db6b4c9bbfdd6777cf3459121
Joshua <jbkunst@gmail.com>
2017-05-09 18:14:08 -0300

    regression

--- clase-06-regresion-arboles/script_regresion.R
+++ clase-06-regresion-arboles/script_regresion.R
@@ -1,63 +1,107 @@
+rm(list = ls())
 library(tidyverse)
-data("anscombe")
+library(stringr)
 
-lm1 <- lm(y1 ~ x1, data = anscombe)
+if("jbkmisc" %in% installed.packages()) {
+  library(jbkmisc)
+  theme_set(theme_jbk())
+}
+
+dfanscombe <- tbl_df(anscombe) %>% 
+  gather(key, value) %>% 
+  separate(key, c("valor", "set"), sep = 1)
+
+dfanscombe <- bind_cols(
+  dfanscombe %>% filter(valor == "x") %>% rename(x = value) %>% select(-set, -valor),
+  dfanscombe %>% filter(valor == "y") %>% rename(y = value) %>% select(-valor)
+ ) %>% 
+  mutate(set2 = factor(set, labels = c("Normal (x1, y1)", "Falta de Ajuste (x2, y2)",
+                                      "Outiler (x3, y3)", "Influencia (x4, y4)")))
+
+dfanscombe <- dfanscombe %>% 
+  arrange(set, x)
+
+dfanscombe
+
+ggplot(dfanscombe, aes(x = x, y = y)) +
+  geom_point(color = "darkred", size = 4, shape = 1) +
+  geom_smooth(method = "lm", color = "navy", alpha = 0.1) +
+  facet_wrap(~set2, scales = "fixed") +
+  labs(x = NULL, y = NULL)
+
+lm1 <- lm(y ~ x, data = filter(dfanscombe, set == 1))
 lm1
 
-lm2 <- lm(y2 ~ x2, data = anscombe)
+lm2 <- lm(y ~ x, data = filter(dfanscombe, set == 2))
 lm2
 
-lm3 <- lm(y3 ~ x3, data = anscombe)
+lm3 <- lm(y ~ x, data = filter(dfanscombe, set == 3))
 lm3
 
-lm4 <- lm(y4 ~ x4, data = anscombe)
+lm4 <- lm(y ~ x, data = filter(dfanscombe, set == 4))
 lm4
 
-anscombe <- anscombe %>% 
+#' Evaluacion de calidad del modelo estudiando errores (residuos)
+dfanscombe <- dfanscombe %>% 
   mutate(
-    res1 = lm1$residuals,
-    res2 = lm2$residuals,
-    res3 = lm3$residuals,
-    res4 = lm4$residuals
+    res = c(lm1$residuals, lm2$residuals, lm3$residuals, lm4$residuals)
   )
 
-ggplot(arrange(anscombe, x1)) + 
-  geom_point(aes(x1, res1))
-
-ggplot(arrange(anscombe, x2)) + 
-  geom_point(aes(x2, res2))
-
-ggplot(arrange(anscombe, x3)) + 
-  geom_point(aes(x3, res3))
-
-ggplot(arrange(anscombe, x4)) + 
-  geom_point(aes(x4, res4))
-
-
-
-# ?!
-summary(lm1)
-summary(lm2)
-summary(lm3)
-summary(lm4)
-
-
-
-x <- rnorm(100)
-y <- 2 + 0.5 * x
-
-n <- rnorm(length(y),s=0.4)
-
-nfit <- lm(n ~ x)
-
-y1 <- y + nfit$residuals
-
-plot(x, n)
-plot(x, y)
-plot(x, y1)
-
-lm(y1 ~ x)
+ggplot(dfanscombe, aes(x = x, y = res)) +
+  geom_point(color = "darkred", size = 4, shape = 1) +
+  geom_smooth(method = "lm", color = "navy", alpha = 0.1) +
+  geom_smooth(method = "loess", color = "skyblue", alpha = 0.1, span = 1, se = FALSE) +
+  facet_wrap(~set2, scales = "fixed") +
+  labs(x = NULL, y = NULL)
 
 
 
 
+# transformaciones --------------------------------------------------------
+df <- data_frame(
+  x = 1:50,
+  y = x^2 + rnorm(length(x), sd = 15)
+)
+df
+
+ggplot(df, aes(x, y)) + 
+  geom_point(shape = 1)
+
+ggplot(df, aes(x, y)) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
+
+ggplot(df, aes(x, log(y))) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
+
+ggplot(df, aes(x, sqrt(y))) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
+
+ggplot(df, aes(x, sqrt(y))) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
+
+
+# heterocedasticidad
+df <- data_frame(
+  x = 1:100,
+  y = 50 +  10 * x + (10 + x^1.2) * rnorm(length(x), sd = 1)
+)
+df
+
+ggplot(df, aes(x, y)) + 
+  geom_point(shape = 1)
+
+ggplot(df, aes(x, y)) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
+
+ggplot(df, aes(x, log(y))) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
+
+ggplot(df, aes(x, sqrt(y))) + 
+  geom_point(shape = 1) +
+  geom_smooth(method = "lm")
