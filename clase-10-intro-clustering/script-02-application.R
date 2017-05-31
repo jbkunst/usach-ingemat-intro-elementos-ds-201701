dfb2ef7b0fe6d6b037b083f10be11e79e939b65d
Joshua Kunst <jbkunst@gmail.com>
2017-05-30 21:57:17 -0400

    Update script-02-application.R

--- clase-10-intro-clustering/script-02-application.R
+++ clase-10-intro-clustering/script-02-application.R
@@ -1,30 +1,77 @@
+# install.packages(c("jpeg", "threejs"))
 library(jpeg)
 library(threejs)
+library(tidyverse)
 
 
-image <- readJPEG(input$image)
+
+image <- readJPEG("82127.jpg")
 image
+
+dim(image)
+dim(image[,,2])
+
+
+
+matrix_to_df <- function(m) {
+  # m <- matrix(round(runif(12), 2), nrow = 4)
+  m %>% 
+    tbl_df() %>% 
+    mutate(y = seq_len(nrow(.))) %>% 
+    gather(x, c, -y) %>% 
+    mutate(
+      x = as.numeric(gsub("V", "", x)),
+      y = rev(y)
+    ) %>% 
+    select(x, y, c)
+  
+}
+
 
 df_image <- map(1:3, function(i) image[,,i]) %>% 
   map(matrix_to_df) %>% 
   reduce(left_join, by = c("x", "y")) %>% 
   rename(r = c.x, g = c.y, b = c) %>% 
   mutate(rgb = rgb(r, g, b)) 
+df_image
+
+
+plot(df_image$x, df_image$y, col = df_image$rgb)
+
+df_image <- mutate(df_image, rgb2 = rgb(r, g, b, 0.8))
+df_image
+
+df_image_sample <- sample_n(df_image, 10000)
+plot(df_image_sample$x, df_image_sample$y, col = df_image_sample$rgb)
+
+df_image_sample <- sample_n(df_image, 1000)
+plot(df_image_sample$x, df_image_sample$y, col = df_image_sample$rgb)
+df_image_sample
+
+scatterplot3js(df_image_sample$r, df_image_sample$g, df_image_sample$b,
+               color = df_image_sample$rgb)
+
+km <- kmeans(df_image %>% select(r, g, b, x, y), 200)
+km$centers
+
+df_centers <- km$centers %>% 
+  as_data_frame() %>% 
+  mutate(cluster = seq(1, nrow(km$centers))) %>% 
+  mutate(rgbc = rgb(r*255, g*255, b*255, maxColorValue = 255)) %>% 
+  select(cluster, rgbc)
+
+df_image <- df_image %>% 
+  mutate(cluster = km$cluster) %>% 
+  left_join(df_centers, by = "cluster")
+
+df_image_sample <- sample_n(df_image, 1000)
+
+scatterplot3js(df_image_sample$r, df_image_sample$g, df_image_sample$b,
+               color = df_image_sample$rgbc)
+
+plot(df_image$x, df_image$y, col = df_image$rgb, axes = FALSE)
+plot(df_image$x, df_image$y, col = df_image$rgbc, axes = FALSE)
 
 
 
-kMeans <- df_image %>% 
-  select(r, g, b) %>% 
-  kmeans(centers = input$k1)
 
-df_image <- df_image %>%
-  mutate(rbgApp = rgb(kMeans$centers[kMeans$cluster, c("r", "g", "b")])) %>% 
-  tbl_df()
-
-plot(df_image$x, df_image$y, col = df_image$rbgApp, asp = 1, pch = ".",
-     ylab = "", xlab = "", xaxt="n", yaxt="n", axes=FALSE)
-
-
-
-scatterplot3js(df_image_aux[,c("red", "green", "blue")], size = log(df_image_aux$proportion*1000),
-               #                  color=df_image_aux$rbgApp, renderer="canvas")
