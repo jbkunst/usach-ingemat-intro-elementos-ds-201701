afe3aa61452066dfaecc2565556820e50dfa3ef2
Joshua <jbkunst@gmail.com>
2017-06-13 17:45:47 -0400

    no va a resultar

--- clase-11-random-forest/script_random_forest_revenge.R
+++ clase-11-random-forest/script_random_forest_revenge.R
@@ -1,39 +1,21 @@
+rm(list = ls())
+library(tidyverse)
+library(stringr)
 library(tidyverse)
 library(partykit)
 
-# data <- read_csv("https://raw.githubusercontent.com/jbkunst/riskr/master/data/data.csv")
-data <- read_csv("train.csv")
-names(data)
+data <- read_csv("https://raw.githubusercontent.com/jadeyee/r2d3-part-1-data/master/part_1_data.csv", skip = 2)
 
-glimpse(data)
+data <- mutate(data, in_sf = ifelse(in_sf == 1, "Y", "N"))
 
-row <- sample(nrow(data), size = 1)
-
-photo <- data_frame(x = rep(1:28, times=28), y = rep(28:1, each = 28),
-                    shade = as.numeric(data[row,-1]))
-
-summary(photo$shade)
-
-ggplot(data=photo) +
-  geom_point(aes(x = x, y = y, color = shade), size=11, shape=15) +
-  scale_color_gradient(low = "white", high = "black") +
-  theme(legend.position = "none")
-  
-# algunos cosas necesarias
-data <- mutate(data, label = factor(label))
-
-# tamohs ready
-glimpse(data)
-
-
-s <- sample(c("train", "test"), size = nrow(data), replace = TRUE)
+s <- sample(c("train", "test"), size = nrow(data), replace = TRUE, c(2, 1))
 
 data_test <- filter(data, s == "train")
 data_train <- filter(data, s == "test")
 
 
 # arbol iluso -------------------------------------------------------------
-mod_ilu <- ctree(label ~ ., data = data_test) #  :O
+mod_ilu <- ctree(as.factor(in_sf) ~ ., data = data_test) #  :O
 plot(mod_ilu)
 plot(mod_ilu, gp = gpar(fontsize = 5)) 
 
@@ -41,37 +23,35 @@ data_test <- data_test %>%
   mutate(mod_ilu_pred = predict(mod_ilu, newdata = data_test))
 
 data_test %>% 
-  group_by(label, mod_ilu_pred) %>%
+  group_by(in_sf, mod_ilu_pred) %>%
   summarise(n = n()) %>% 
   ungroup() %>% 
-  mutate(que_pasho = ifelse(label == mod_ilu_pred, ":)", ":(")) %>% 
+  mutate(que_pasho = ifelse(in_sf == mod_ilu_pred, ":)", ":(")) %>% 
   group_by(que_pasho) %>% 
   summarise(n = sum(n)) %>% 
   mutate(tasa_clasificacion_mundial = n/sum(n))
 
-
 data_test %>% 
-  group_by(label, mod_ilu_pred) %>%
+  group_by(in_sf, mod_ilu_pred) %>%
   summarise(n = n()) %>% 
   spread(mod_ilu_pred, n) %>% 
   ungroup() 
 
 # mod bagging -------------------------------------------------------------
-M <- 50 
+M <- 500 
 dim(data)
 P <- round(sqrt(ncol(data_train)))
 
 haceme_un_arbolito <- function() {
   
-  
   p <- sample(2:ncol(data), size = P)
   print(names(data_train)[p])
   
   data_pelotita <- data_train %>% 
-    select(p, label) %>% 
+    select(p, in_sf) %>% 
     sample_n(nrow(data_train), replace = TRUE)
   
-  arbol_pelotita <- ctree(label ~ ., data_pelotita) #, control = ctree_control(maxdepth = 5))
+  arbol_pelotita <- ctree(as.factor(in_sf) ~ ., data_pelotita, control = ctree_control(maxdepth = 5))
   
   # plot(arbol_pelotita, gp = gpar(fontsize = 5)) 
   
@@ -84,15 +64,14 @@ haceme_un_arbolito()
 bosque <- list()
 
 
-
 for(i in 1:M) {
   
   message("Yay! iteracion ", i)
   
   mod <- haceme_un_arbolito()
-
+  
   bosque[i] <- list(mod)
-    
+  
 }
 
 # bosque
@@ -103,6 +82,7 @@ df_pred <- map(1:M, function(i){
   mod <- bosque[[i]]
   
   data_test %>% 
+    # mutate(pred = predict(mod, newdata = data_test, type = "prob")[,1]) %>%
     mutate(pred = predict(mod, newdata = data_test)) %>% 
     select(pred) %>% 
     mutate(id = 1:nrow(data_test), arbolito = i)
@@ -116,39 +96,28 @@ na_a_0 <- function(x) ifelse(is.na(x), 0, x)
 na_a_0(c(NA, 1, 2,3,4, NA))
 
 data_test %>% 
-  count(label) %>% 
+  count(in_sf) %>% 
   mutate(p = n/sum(n))
 
 df_pred_f <- df_pred %>% 
-  group_by(id) %>% 
-  count(pred) %>% 
-  spread(pred, n) %>%
-  ungroup() %>% 
-  mutate_all(na_a_0)
+  count(id, pred) %>% 
+  spread(pred, n)
 
-df_pred_f
-
-df_pred_f <- df_pred %>% 
-  group_by(id) %>% 
-  count(pred) %>%
-  group_by(id) %>% 
-  arrange(id, desc(n)) %>% 
-  distinct(id, .keep_all = TRUE) %>% 
-  left_join(df_pred_f, ., by = "id")
-  
-df_pred_f
-  
-  
 data_test2 <- data_test %>% 
-  select(label) %>% 
-  bind_cols(df_pred_f)
+  select(in_sf) %>% 
+  bind_cols(df_pred_f) %>% 
+  ungroup() %>% 
+  mutate_all(na_a_0) %>% 
+  mutate(pred = ifelse(N >= Y, "N", "Y"))
 
 glimpse(data_test2)
 
+data_test2
+
 data_test2 %>% 
-  group_by(label, pred) %>%
+  group_by(in_sf, pred) %>%
   summarise(n = n()) %>% 
-  mutate(que_pasho = ifelse(label == pred, ":)", ":(")) %>% 
+  mutate(que_pasho = ifelse(in_sf == pred, ":)", ":(")) %>% 
   group_by(que_pasho) %>% 
   summarise(n = sum(n)) %>% 
   mutate(tasa_clasificacion_mundial = n/sum(n))
@@ -159,7 +128,7 @@ library(randomForest)
 
 data_train <- filter(data_train, complete.cases(data_train))
 
-bosque_verdad <- randomForest(label ~ ., data = data_train, ntree = M, do.trace = TRUE)
+bosque_verdad <- randomForest(as.factor(in_sf) ~ ., data = data_train, ntree = M, do.trace = TRUE)
 
 predict(bosque_verdad, newdata = data_test)
 
@@ -167,16 +136,16 @@ data_test <- data_test %>%
   mutate(mod_rf = predict(bosque_verdad, newdata = data_test))
 
 data_test %>% 
-  group_by(label, mod_rf) %>%
+  group_by(in_sf, mod_rf) %>%
   summarise(n = n()) %>% 
-  mutate(que_pasho = ifelse(label == mod_rf, ":)", ":(")) %>% 
+  mutate(que_pasho = ifelse(in_sf == mod_rf, ":)", ":(")) %>% 
   group_by(que_pasho) %>% 
   summarise(n = sum(n)) %>% 
   mutate(tasa_clasificacion_mundial = n/sum(n))
 
 
 data_test %>% 
-  group_by(label, mod_rf) %>%
+  group_by(in_sf, mod_rf) %>%
   summarise(n = n()) %>% 
   spread(mod_rf, n) %>% 
   ungroup() 
